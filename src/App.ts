3d924851bcf9bcca8f9a6524a8070fe63b35dc71
Zenlabs Mac2 <zenlabsmac2@Zenlabss-iMac2.local>
2017-08-31 10:16:01 +0530

    Added invoking a remote web service

--- src/App.ts
+++ src/App.ts
@@ -13,7 +13,7 @@ class App {
 
   //Run configuration methods on the Express instance.
   constructor() {
-    this.express = express();   
+    this.express = express();
     this.middleware();
     this.routes();
   }
@@ -32,39 +32,58 @@ class App {
      * API endpoints */
     let router = express.Router();
     // placeholder route handler
-    router.get('/', (req: express.Request , res: express.Response, next: express.NextFunction) => {
-      
+    router.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+
       res.json({
         message: 'its working'
       });
     });
 
-    router.post('/', (req: express.Request , res: express.Response, next: express.NextFunction) => {
-     console.log(JSON.stringify(req.body))
-     const condtionLeft:any[] = jsonpath.query(req.body,'$.result.action' );
-     console.log(JSON.stringify(condtionLeft));
-      if(condtionLeft.indexOf( 'AddNumbers') >-1){
-        const nums = jsonpath.query(req.body,'$.result.parameters.a' )[0];
+    router.post('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      console.log(JSON.stringify(req.body))
+      const condtionLeft: any[] = jsonpath.query(req.body, '$.result.action');
+      console.log(JSON.stringify(condtionLeft));
+      if (condtionLeft.indexOf('AddNumbers') > -1) {
+        const nums = jsonpath.query(req.body, '$.result.parameters.a')[0];
         console.log(JSON.stringify(nums));
-        remoteRequest.post('http://localhost:8080/sum',nums)
-        .on('response', function(serviceResponse){
-          console.log('DATA returned:'+JSON.stringify(serviceResponse));
-        });
-        
-       const total = nums.reduce((sum,current) => sum+current);
-        res.json({
-          speech: `The total Amount is ${total}` ,
-          displayText: `The total Amount is ${total}` ,
-          source:'My Tool'
+        let requestOptions: remoteRequest.Options = {
+          method: 'POST',
+          qs: {},
+          headers: {},
+          uri: 'https://mathservice.herokuapp.com/sum',
+          useQuerystring: false,
+          json: true,
+          body: nums,
+        };
+        remoteRequest(requestOptions, (error, response, body) => {
+          if (error) {
+            console.log(JSON.stringify(error));
+          } else {
+            if (response.statusCode >= 200 && response.statusCode <= 299) {
+              console.log(JSON.stringify(response));
+              console.log(JSON.stringify(body));
+              res.json({
+                speech: `The total Amount is ${body}`,
+                displayText: `The total Amount is ${body}`,
+                source: 'Math Service'
 
+              })
+            } else {
+              console.log('Error' + JSON.stringify(response));
+            }
+          }
+        });
+
+
+        const total = nums.reduce((sum, current) => sum + current);
+        ;
+      }
+      else {
+        res.json({
+          message: 'its working -' + req.body['result']['action'] + ' - '
+          + req.body['result']['parameters']
         });
       }
-      else{
-      res.json({
-        message: 'its working -'+ req.body['result']['action'] + ' - '
-        +req.body['result']['parameters']
-      });
-    }
     })
     this.express.use('/', router);
   }
