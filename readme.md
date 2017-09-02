b62fbc5ad35aa3c0606b6a8fd5931d5ccdc7b586
Srini <srinivasarajui@gmail.com>
2017-09-02 20:27:53 +0530

    Updated Materics

--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 # Fulfilment Router
 
-Using CircleCli to build
+Using CircleCli to build [![CircleCI](https://circleci.com/gh/srinivasarajui/FulfilmentRouter.svg?style=svg)](https://circleci.com/gh/srinivasarajui/FulfilmentRouter)
 
-Creating messangerapptraining1... done
-https://messangerapptraining1.herokuapp.com/ | https://git.heroku.com/messangerapptraining1.git
+
+Heroku URL
+https://messangerapptraining1.herokuapp.com/ 
+Heroku Git
+https://git.heroku.com/messangerapptraining1.git
--- src/routes/MetricsService.ts
+++ src/routes/MetricsService.ts
@@ -1,11 +1,15 @@
 
 import * as express from 'express';
+import * as Process from 'process';
+export class MetricsService {
 
-export class MetricsService{
-
-  public   metrics(req: express.Request, res: express.Response, next: express.NextFunction):void {
-        res.json({
-           message: 'its working'
-         });
-       }
+  public metrics(req: express.Request, res: express.Response, next: express.NextFunction): void {
+    res.status(200)
+    .json({
+      cpuUsage: Process.cpuUsage(),
+      memoryUsage: Process.memoryUsage(),
+      upTime: Process.uptime(),
+      status: 'UP'
+    }).end();
+  }
 }
