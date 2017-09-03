14373d7a27fe736b648de575ea80ccc775b138a6
Srini <srinivasarajui@gmail.com>
2017-09-03 09:30:23 +0530

    Changes have no impact

--- src/Request.ts
+++ src/Request.ts
@@ -1,8 +1,12 @@
 /* As of Now not used */
+export class Result{
+
+}
 export class Request{
     id:String;
     timestamp:String;
     lang: String;
     sessionId: String;
+    result:Result;
     
 }
