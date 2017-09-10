32b88076a9df11590233543f482c278d27d7cf72
Srini <srinivasarajui@gmail.com>
2017-09-11 00:19:05 +0530

    Add Testing Code

---
+++ test/apiroute.test.ts
@@ -0,0 +1,38 @@
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import app from '../src/App';
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+interface TestDetails{
+    [key: string]: {
+        expectedText:string,
+        input:any
+    }
+}
+describe('API Route', function() {
+    this.timeout(15000);
+    const testData = <TestDetails>require('../test/APIServiceTest.json');
+    for(const key in testData) {
+     
+      it(key, done  => {
+         
+        chai.request(app).post('/api')
+        .send(testData[key].input)
+        .auth('user','password')
+        .end((err, res) => {
+          expect(res.status).to.eql(200);
+          expect(res.type).to.eql('application/json');
+          expect(res.body.speech).to.eql(testData[key].expectedText);
+          expect(res.body.displayText).to.eql(testData[key].expectedText);
+          expect(res.body.source).to.eql('MathService');
+         
+          done();
+        });
+      });
+    
+    }
+    
+    });
